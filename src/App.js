eef53d21c0c2eee8667c424f320c2fe0f288407c
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2023-10-22 18:18:44 +0530

    ho gaya

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import GetData from "./components/GetData";
 import { Route, Routes } from "react-router-dom";
 import Product from "./components/Product";
+import MockUp from "./components/MockUp";
 
 const App = () => {
   return (
     <div>
       
-      <Routes>
+      {/* <Routes>
         <Route path="/" exact element={<GetData/>} />
         <Route path="/product/:id" element={<Product />} />
-      </Routes>
+      </Routes> */}
 
-      
+      <MockUp/>
     </div>
   );
 };
