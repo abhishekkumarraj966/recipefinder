6b154914a9fb34bfb794969fdfdf8804e0bc2487
Abhishek Kumar Raj <abhishekkumarraj62073@gmail.com>
2023-10-23 18:11:56 +0530

    Add a card

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -29,47 +29,77 @@ const GetData = () => {
 
   return (
     <div>
-      <form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+      <form>
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
             </svg>
-        </div>
-        <input type="search" value={inputValue} onChange={handleInputChange} id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search on Recap" required />
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
-
-      {/* <h2>Posts:</h2> */}
-      {/* write the code to fetch data from api */}
-
-      <div>
-        {data.map((post) => (
-          <div>
-            <Link to={`/product/${post.id}`} key={post.id}>
-              <div key={post.id}>
-                <div className="drop-shadow-lg   w-80 mb-5">
-                  <div>
-                    {/* <img className=" h-80" src={post.image} alt="title" /> */}
-
-                    <img
-          className=" h-80"
-            src="https://static.toiimg.com/thumb/52554168.cms?width=1200&height=900"
-            alt="title"
+          </div>
+          <input
+            type="search"
+            value={inputValue}
+            onChange={handleInputChange}
+            id="default-search"
+            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search on Recap"
+            required
           />
-                  </div>
-                  <div className="">
-                    <h1 className=" py-2 text-center text-3xl ">
-                      {post.title}
-                    </h1>
+          <button
+            type="submit"
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      <div class="w-full p-4 text-center bg-white border border-gray-100 rounded-lg shadow sm:p-8 ">
+        <h5 class="mb-2 text-3xl font-bold   text-red-900">Shop By Gender</h5>
+        <p class="mb-5 text-base sm:text-lg  text-gray-900">
+          first class jewellery for fast-day for Min Kide - women
+        </p>
+        <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+          <div className="flex flex-row flex-wrap justify-center mb-20">
+            {data.map((post) => (
+              <Link to={`/product/${post.id}`} key={post.id}>
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  mx-3 ">
+                  <a href="/">
+                    <img
+                      className="rounded-t-lg h-80"
+                      src={post.image}
+                      alt=""
+                    />
+                  </a>
+                  <div className="p-5">
+                    <div className="flex justify-between hover:justify-start">
+                      <div className="">
+                        <h1 className="">{post.title}</h1>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -35,21 +35,49 @@ const Product = (props) => {
     //   <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     // </div>
 
-    <div>
-      <div className="flex flex-col justify-center px-10">
-        <img className=" rounded-lg" src={data.image} alt="foodImage" />
+    // <div>
+    //   <div className="flex flex-col justify-center px-10 h-auto w-30">
+    //     <img className=" rounded-lg" src={data.image} alt="foodImage" />
 
-        {/* <img
-          className=" h-80"
-            src="https://static.toiimg.com/thumb/52554168.cms?width=1200&height=900"
-            alt="title"
-          /> */}
-      </div>
-      <div>
-        <h1 className=" font-bold text-3xl mb-5 text-center">{data.title} </h1>
-      </div>
-      <div className="text-center">
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    //     {/* <img
+    //       className=" h-80"
+    //         src="https://static.toiimg.com/thumb/52554168.cms?width=1200&height=900"
+    //         alt="title"
+    //       /> */}
+    //   </div>
+    //   <div>
+    //     <h1 className=" font-bold text-3xl mb-5 text-center">{data.title} </h1>
+    //   </div>
+    //   <div className="text-center">
+    //     <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    // </div>
+    // </div>
+
+    <div class="w-full p-4 text-center bg-white border border-gray-100 rounded-lg shadow sm:p-8 ">
+      <h5 class="mb-2 text-3xl font-bold   text-red-900">Shop By Gender</h5>
+      <p class="mb-5 text-base sm:text-lg  text-gray-900">
+        first class jewellery for fast-day for Min Kide - women
+      </p>
+      <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+        <div className="flex flex-row flex-wrap justify-center">
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  mx-3 ">
+            <a href="/">
+              <img className="rounded-t-lg h-80" src={data.image} alt="" />
+            </a>
+            <div className="p-5">
+              <div className="flex justify-between hover:justify-start">
+                <div className="">
+                  <h1 className="">{data.title}</h1>
+                </div>
+
+                <div>
+                  <span>Explore</span>
+                  <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
